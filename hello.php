488699310c42b86af6344791b35bc607f934f65c
nooralam <nooralam@gmail.com>
2019-08-28 16:28:02 +0600

    new class has been added to this file

--- hello.php
+++ hello.php
@@ -6,4 +6,13 @@ function hello(){
     echo "Hellow world future";
 }
 hello();
+
+class Database{
+    
+    public function __construct(){
+        
+        echo "this is Database class for connect database in this file";
+    }
+}
+
 ?>
